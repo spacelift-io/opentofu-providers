19d0447c3ae06e53445b3b67098bea1367c10233
Michał Goliński <michalg@spacelift.io>
2025-09-09 19:06:40 +0200

    Plan and Validate funtions

--- tofuprovider/internal/tf5/managed_resource.go
+++ tofuprovider/internal/tf5/managed_resource.go
@@ -7,6 +7,7 @@ import (
 	"github.com/apparentlymart/opentofu-providers/tofuprovider/grpc/tfplugin5"
 	"github.com/apparentlymart/opentofu-providers/tofuprovider/internal/common"
 	"github.com/apparentlymart/opentofu-providers/tofuprovider/providerops"
+	"github.com/apparentlymart/opentofu-providers/tofuprovider/providerschema"
 )
 
 // ApplyManagedResourceChange implements tofuprovider.GRPCPluginProvider.
@@ -26,7 +27,42 @@ func (p *Provider) MoveManagedResourceState(ctx context.Context, req *providerop
 
 // PlanManagedResourceChange implements tofuprovider.GRPCPluginProvider.
 func (p *Provider) PlanManagedResourceChange(ctx context.Context, req *providerops.PlanManagedResourceChangeRequest) (providerops.PlanManagedResourceChangeResponse, error) {
-	panic("unimplemented")
+	priorState, err := makeDynamicValueMsgpack(req.PriorState)
+	if err != nil {
+		return nil, fmt.Errorf("invalid PriorState value: %w", err)
+	}
+	proposedNewState, err := makeDynamicValueMsgpack(req.ProposedNewState)
+	if err != nil {
+		return nil, fmt.Errorf("invalid ProposedNewState value: %w", err)
+	}
+	config, err := makeDynamicValueMsgpack(req.Config)
+	if err != nil {
+		return nil, fmt.Errorf("invalid Config value: %w", err)
+	}
+
+	var providerMeta *tfplugin5.DynamicValue
+	if req.ProviderMeta != providerschema.NoDynamicValue {
+		providerMeta, err = makeDynamicValueMsgpack(req.ProviderMeta)
+		if err != nil {
+			return nil, fmt.Errorf("invalid ProviderMeta value: %w", err)
+		}
+	}
+
+	protoReq := &tfplugin5.PlanResourceChange_Request{
+		TypeName:           req.ResourceType,
+		PriorState:         priorState,
+		ProposedNewState:   proposedNewState,
+		Config:             config,
+		PriorPrivate:       req.PriorProviderInternal,
+		ProviderMeta:       providerMeta,
+		ClientCapabilities: prepareClientCapabilities(req.ClientCapabilities),
+	}
+
+	protoResp, err := p.client.PlanResourceChange(ctx, protoReq)
+	if err != nil {
+		return nil, err
+	}
+	return planManagedResourceChangeResponse{proto: protoResp}, nil
 }
 
 // ReadManagedResource implements tofuprovider.GRPCPluginProvider.
@@ -65,4 +101,37 @@ type validateManagedResourceConfigResponse struct {
 // Diagnostics implements providerops.ValidateEphemeralResourceConfigResponse.
 func (v validateManagedResourceConfigResponse) Diagnostics() providerops.Diagnostics {
 	return diagnostics{proto: v.proto.Diagnostics}
+}
+
+type planManagedResourceChangeResponse struct {
+	proto *tfplugin5.PlanResourceChange_Response
+	common.SealedImpl
+}
+
+// Diagnostics implements providerops.PlanManagedResourceChangeResponse.
+func (p planManagedResourceChangeResponse) Diagnostics() providerops.Diagnostics {
+	return diagnostics{proto: p.proto.Diagnostics}
+}
+
+// PlannedNewState implements providerops.PlanManagedResourceChangeResponse.
+func (p planManagedResourceChangeResponse) PlannedNewState() providerschema.DynamicValueOut {
+	return dynamicValue{proto: p.proto.PlannedState}
+}
+
+// PlannedProviderInternal implements providerops.PlanManagedResourceChangeResponse.
+func (p planManagedResourceChangeResponse) PlannedProviderInternal() []byte {
+	return p.proto.PlannedPrivate
+}
+
+// LegacyTypeSystem implements providerops.PlanManagedResourceChangeResponse.
+func (p planManagedResourceChangeResponse) LegacyTypeSystem() bool {
+	return p.proto.LegacyTypeSystem
+}
+
+// Deferred implements providerops.PlanManagedResourceChangeResponse.
+func (p planManagedResourceChangeResponse) Deferred() providerops.Deferred {
+	if p.proto.Deferred == nil {
+		return nil
+	}
+	return deferred{proto: p.proto.Deferred}
 }
